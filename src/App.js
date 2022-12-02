177d00f261ad1b5d36641305c448f3dd07223cbb
babyAdrian1880 <104036692+babyAdrian1880@users.noreply.github.com>
2022-12-01 22:03:52 -0600

    AnotherUpdate12_02_2022

--- src/App.js
+++ src/App.js
@@ -6,23 +6,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchPage from './Components/SearchPage';
 
 
-export default function App() {
+function App() {
   
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route exact path="./" element={<Home/>}>
-
-          </Route>
-          <Route path="./search" element={<SearchPage/>}>
-
-          </Route>
-        </Routes>
-      </Router>
+    <div className="App">
       <Home />
     </div>
   );
 }
 
-//export default App;
+export default App;
