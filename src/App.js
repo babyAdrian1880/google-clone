339826f0b9642f55f1d8b4f1a86267f6abdaa5df
babyAdrian1880 <104036692+babyAdrian1880@users.noreply.github.com>
2022-12-02 02:06:41 -0600

    AnotherUpdate

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 import Home from './Components/Home'
 import React from 'react';
 
-function App() {
-  
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchPage from './Components/SearchPage';
+
+
+export default function App() {
+
   return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+    <div>
+    <Router>
+      <Routes>
+        <Route exact path="./" element={<Home/>}>
+
+        </Route>
+        <Route path="./search" element={<SearchPage/>}>
+
+        </Route>
+      </Routes>
+    </Router>
+  <Home />
+  </div>
+);
 }
 
-export default App;
+//export default App;
